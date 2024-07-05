2b7422d66dfca02112f1d131f0af6b186216b8a3
Pranjali bramhankar <pranjalibramhankar14@gmail.com>
2024-07-05 18:59:21 +0530

    updaated ecomerce page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({ color, bgColor, text, borderRadius, size }) => {
   return (
     <div>
-      
+      <button
+        type="button"
+        style={{
+          backgroundColor: bgColor,
+          color,
+
+          text,
+          borderRadius,
+        }}
+        className={`{text-${size} p-3 hover:drop-shadow-xl`}
+      >
+        {text}
+      </button>
     </div>
   )
 }
--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from '@syncfusion/ej2-react-charts'
 
-const SparkLine = () => {
+const SparkLine = ({ currentColor, id, type, height, width, data, color }) => {
+  console.log(`Sparkline type: ${type}`)
   return (
-    <div>
-      
-    </div>
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName="x"
+      yName="y"
+      type={type}
+    >
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
   )
 }
 
