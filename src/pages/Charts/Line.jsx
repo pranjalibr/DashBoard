f8d1abda65d200f29dc5f41da5a2ad0dd30492ac
Pranjali bramhankar <pranjalibramhankar14@gmail.com>
2024-07-09 16:17:16 +0530

    updaated Area bar page

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Header, Linechart } from '../../components'
 
 const Line = () => {
   return (
-    <div>
-      
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Chart" title="Inflation Rate" />
+      <div className="w-full">
+        <Linechart />
+      </div>
     </div>
   )
 }
